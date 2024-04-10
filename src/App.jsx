9d1901f6c3b267019c7f793c93a376c23a17f351
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-10 16:19:05 +0000

    created homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
 import "./App.css";
-import Header from "./components/Header"
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Link } from "react-router-dom";
 
 export default function App() {
   return (
-    <main>
+    <>
       <Header />
-    </main>
+      <main className="container my-5">
+        <div className="my-5">
+          <img
+            src="https://placehold.co/400x300?text=Employees Smiling"
+            alt="company employees"
+            className="img-fluid"
+          />
+        </div>
+        <div className="my-5">
+          <h2>Our Employees</h2>
+          <p>
+            Meet our dedicated team of professionals who work tirelessly to
+            achieve our company's goals.
+          </p>
+          <Link to="/employees" className="btn btn-primary">
+            View Employees
+          </Link>
+        </div>
+        <div className="my-5">
+          <h2>Company Report</h2>
+          <p>
+            Explore our latest financial report to gain insights into our
+            company's performance and growth.
+          </p>
+          <Link to="/report" className="btn btn-primary">
+            View Report
+          </Link>
+        </div>
+      </main>
+      <Footer/>
+    </>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
+  </React.StrictMode>
 );
