b04c43533f66787d85569a67c97459d77dddbbd6
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-10 17:16:51 +0000

    created employeList page

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-
+import EmployeeList from "./pages/EmployeeList";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>
+    element: <App />,
+  },
+  {
+    path: "/employees",
+    element: <EmployeeList />,
+  },
+  {
+    path : "/employees/:empId",
+    element : <EmployeeDetails/>
   }
-])
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <RouterProvider router={router}/>
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
